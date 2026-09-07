6b3170b4cde95e842e226e8346573b026e7917bb
nahdirj <102171695+nahdirj@users.noreply.github.com>
2026-09-07 20:05:52 +0100

    working JS file

--- script.js
+++ script.js
@@ -1,10 +1,35 @@
 function computerPlay() {
   const options = ["rock", "paper", "scissors"];
   const number = Math.floor(Math.random() * 3);
-  return options[number];
+
+  var Fred = options[number];
+  return Fred;
+  console.log(Fred);
 }
 
-console.log(computerPlay());
+//var computerSelection = options[number];
 
-function playRound(playerSelection,computerSelection){if (playerSelection===computerSelection) return "Draw"} 
-if {(playerSelection==="rock") && (computerSelection === "paper") || (computerSelection==="scissors"){ return "computer has won"}}
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection === computerSelection) {
+    return "Draw";
+  }
+
+  if (
+    (playerSelection === "scissors" && computerSelection === "rock") ||
+    (playerSelection === "paper" && computerSelection === "rock") ||
+    (playerSelection === "scissors" && computerSelection === "paper")
+  ) {
+    return "Player has won";
+  }
+
+  return "Computer has won";
+}
+
+const playerSelection = prompt("Choose rock, paper, or scissors:");
+console.log(playerSelection);
+if (playerSelection !== null) {
+  const computerSelection = computerPlay();
+  const result = playRound(playerSelection.toLowerCase(), computerSelection);
+  console.log(computerSelection);
+  console.log(result);
+}
